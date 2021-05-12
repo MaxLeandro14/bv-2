ac6101fb343be57d944ef882756c91aca80d3a1d
maxleandro14 <maxleandro2@gmail.com>
2021-05-12 14:33:51 -0300

    first commits4

--- pages/index.vue
+++ pages/index.vue
@@ -65,7 +65,216 @@
             <v-row>
               
               <div class="col-6 d-flex pacote-preco" style="flex-direction: column; justify-content: center;">
+                <span class="pacote-preco-item">Individual<br><span class="pacote-preco-tipo"><span class="pacote-preco-moeda">R$</span> 2.900,00</span></span>
+                <span class="pacote-preco-item">Casadinha<br><span class="pacote-preco-tipo"><span class="pacote-preco-moeda">R$</span> 900,00</span></span>
+              </div>
+              <div class="col-6 d-flex align-center" style="flex-direction: column; justify-content: center;">
+
+                <v-btn
+                 
+                  class="btn-roteiro"
+                  color="color-roteiro"
+                  @click="openRoteiro('<h1>Carolina</h1>')"
+                >
+                  Descrição
+                  <v-icon right>
+                    mdi-palm-tree
+                  </v-icon>
+                </v-btn>
+
+                <v-btn
+               
+                  color="success"
+                  class="btn-roteiro"
+                >
+                  Whatsapp
+                </v-btn>
+              
+              </div>
+            </v-row>
+            
+           </v-card-text>
+
+        </v-card>
+      
+    </v-col>
+    <!-- -->
+    <!---->
+    <v-col md="4">
+      
+        <v-card
+          class="mx-auto"
+          max-width="320"
+        >
+          <v-img
+            height="200"
+            src="https://i.pinimg.com/originals/d9/f1/5e/d9f15e78f0ac37de87d9b95b545ed03d.jpg"
+          >
+          </v-img>
+          <div class="back-pacote-cinza">
+            <v-row>
+              <div class="col-12">
+                <div class="d-flex align-center justify-space-between">
+                  <span class="pacote-data-principal">
+                    <span class="pacote-data-prox">Próxima Data:</span><br>
+                    2 maio
+                  </span>
+                  <v-btn
+                 
+                    outlined
+                    fab
+                    color="teal"
+                    
+                  >
+                    <v-icon>mdi-playlist-plus</v-icon>
+                  </v-btn>
+                </div>
+              </div>
+            </v-row>
+          </div>
+          <v-card-text>
+            <v-row>
+              <div class="col-12">
+                <p class="pacote-p pacote-title">Atins - Vassouras e Caburé MA</p>
+                <p class="pacote-p pacote-sub">Saindo de <span class="pacote-cidade">São Luís</span></p>
+              </div>
+            </v-row>
+            <v-row>
+              <div class="col-12">
+                <ul class="pacote-list-itens" >
+                  <li>
+                    <span><v-icon class="icon-color">mdi-clock</v-icon></span>
+                    1 Dia
+                  </li>
+                  <li>
+                    <span><v-icon class="icon-color">mdi-clock</v-icon></span>
+                    1 Dia
+                  </li>
+                  <li>
+                    <span><v-icon class="icon-color">mdi-clock</v-icon></span>
+                    1 Dia
+                  </li>
+                  <li>
+                    <span><v-icon class="icon-color">mdi-clock</v-icon></span>
+                    1 Dia
+                  </li>
+                  <li>
+                    <span><v-icon class="icon-color">mdi-clock</v-icon></span>
+                    1 Dia
+                  </li>
+                  <li>
+                    <span><v-icon class="icon-color">mdi-clock</v-icon></span>
+                    1 Dia
+                  </li>
+                </ul>
+              </div>
+            </v-row>
+            <v-row>
+              
+            </v-row>
+            <v-row>
+              
+              <div class="col-6 d-flex pacote-preco" style="flex-direction: column; justify-content: center;">
+                <span class="pacote-preco-item">Quarto Triplo<br><span class="pacote-preco-tipo"><span class="pacote-preco-moeda">R$</span> 2900</span></span>
+                <span class="pacote-preco-item">Quarto Casal<br><span class="pacote-preco-tipo"><span class="pacote-preco-moeda">R$</span> 2900</span></span>
+              </div>
+              <div class="col-6 d-flex align-center" style="flex-direction: column; justify-content: center;">
+
+                <v-btn
+                 
+                  class="btn-roteiro"
+                  color="color-roteiro"
+                  @click="openRoteiro('<h1>Carolina</h1>')"
+                >
+                  Descrição
+                  <v-icon right>
+                    mdi-palm-tree
+                  </v-icon>
+                </v-btn>
+
+                <v-btn
+               
+                  color="success"
+                  class="btn-roteiro"
+                >
+                  Whatsapp
+                </v-btn>
+              
+              </div>
+            </v-row>
+            
+           </v-card-text>
+
+        </v-card>
+      
+    </v-col>
+    <!-- -->
+    <!---->
+    <v-col md="4">
+      
+        <v-card
+          class="mx-auto"
+          max-width="320"
+        >
+          <v-img
+            height="200"
+            src="https://i.pinimg.com/originals/d9/f1/5e/d9f15e78f0ac37de87d9b95b545ed03d.jpg"
+          >
+          </v-img>
+          <div class="back-pacote-cinza">
+            <v-row>
+              <div class="col-12">
+                <div class="d-flex align-center justify-space-between">
+                  <span class="pacote-data-principal">
+                    <span class="pacote-data-prox">Próxima Data:</span><br>
+                    2 maio
+                  </span>
+                  <v-btn
+                 
+                    outlined
+                    fab
+                    color="teal"
+                    
+                  >
+                    <v-icon>mdi-playlist-plus</v-icon>
+                  </v-btn>
+                </div>
+              </div>
+            </v-row>
+          </div>
+          <v-card-text>
+            <v-row>
+              <div class="col-12">
+                <p class="pacote-p pacote-title">Atins - Vassouras e Caburé Maranhão - Interior</p>
+                <p class="pacote-p pacote-sub">Saindo de <span class="pacote-cidade">São Luís</span></p>
+              </div>
+            </v-row>
+            <v-row>
+              <div class="col-12">
+                <ul class="pacote-list-itens" >
+                  <li>
+                    <span><v-icon class="icon-color">mdi-clock</v-icon></span>
+                    1 Dia
+                  </li>
+                  <li>
+                    <span><v-icon class="icon-color">mdi-clock</v-icon></span>
+                    1 Dia
+                  </li>
+                  <li>
+                    <span><v-icon class="icon-color">mdi-clock</v-icon></span>
+                    1 Dia
+                  </li>
+                </ul>
+              </div>
+            </v-row>
+            <v-row>
+              
+            </v-row>
+            <v-row>
+              
+              <div class="col-6 d-flex pacote-preco" style="flex-direction: column; justify-content: center;">
                 <span class="pacote-preco-item">Individual<br><span class="pacote-preco-tipo"><span class="pacote-preco-moeda">R$</span> 2900</span></span>
+                <span class="pacote-preco-item">Casadinha<br><span class="pacote-preco-tipo"><span class="pacote-preco-moeda">R$</span> 2900</span></span>
                 <span class="pacote-preco-item">Casadinha<br><span class="pacote-preco-tipo"><span class="pacote-preco-moeda">R$</span> 2900</span></span>
               </div>
               <div class="col-6 d-flex align-center" style="flex-direction: column; justify-content: center;">
@@ -164,203 +373,6 @@
               
               <div class="col-6 d-flex pacote-preco" style="flex-direction: column; justify-content: center;">
                 <span class="pacote-preco-item">Individual<br><span class="pacote-preco-tipo"><span class="pacote-preco-moeda">R$</span> 2900</span></span>
-                <span class="pacote-preco-item">Casadinha<br><span class="pacote-preco-tipo"><span class="pacote-preco-moeda">R$</span> 2900</span></span>
-              </div>
-              <div class="col-6 d-flex align-center" style="flex-direction: column; justify-content: center;">
-
-                <v-btn
-                 
-                  class="btn-roteiro"
-                  color="color-roteiro"
-                  @click="openRoteiro('<h1>Carolina</h1>')"
-                >
-                  Descrição
-                  <v-icon right>
-                    mdi-palm-tree
-                  </v-icon>
-                </v-btn>
-
-                <v-btn
-               
-                  color="success"
-                  class="btn-roteiro"
-                >
-                  Whatsapp
-                </v-btn>
-              
-              </div>
-            </v-row>
-            
-           </v-card-text>
-
-        </v-card>
-      
-    </v-col>
-    <!-- -->
-    <!---->
-    <v-col md="4">
-      
-        <v-card
-          class="mx-auto"
-          max-width="320"
-        >
-          <v-img
-            height="200"
-            src="https://i.pinimg.com/originals/d9/f1/5e/d9f15e78f0ac37de87d9b95b545ed03d.jpg"
-          >
-          </v-img>
-          <div class="back-pacote-cinza">
-            <v-row>
-              <div class="col-12">
-                <div class="d-flex align-center justify-space-between">
-                  <span class="pacote-data-principal">
-                    <span class="pacote-data-prox">Próxima Data:</span><br>
-                    2 maio
-                  </span>
-                  <v-btn
-                 
-                    outlined
-                    fab
-                    color="teal"
-                    
-                  >
-                    <v-icon>mdi-playlist-plus</v-icon>
-                  </v-btn>
-                </div>
-              </div>
-            </v-row>
-          </div>
-          <v-card-text>
-            <v-row>
-              <div class="col-12">
-                <p class="pacote-p pacote-title">Santo Amaro</p>
-                <p class="pacote-p pacote-sub">Saindo de <span class="pacote-cidade">São Luís</span></p>
-              </div>
-            </v-row>
-            <v-row>
-              <div class="col-12">
-                <ul class="pacote-list-itens" >
-                  <li>
-                    <span><v-icon class="icon-color">mdi-clock</v-icon></span>
-                    1 Dia
-                  </li>
-                  <li>
-                    <span><v-icon class="icon-color">mdi-clock</v-icon></span>
-                    1 Dia
-                  </li>
-                  <li>
-                    <span><v-icon class="icon-color">mdi-clock</v-icon></span>
-                    1 Dia
-                  </li>
-                </ul>
-              </div>
-            </v-row>
-            <v-row>
-              
-            </v-row>
-            <v-row>
-              
-              <div class="col-6 d-flex pacote-preco" style="flex-direction: column; justify-content: center;">
-                <span class="pacote-preco-item">Individual<br><span class="pacote-preco-tipo"><span class="pacote-preco-moeda">R$</span> 2900</span></span>
-                <span class="pacote-preco-item">Casadinha<br><span class="pacote-preco-tipo"><span class="pacote-preco-moeda">R$</span> 2900</span></span>
-              </div>
-              <div class="col-6 d-flex align-center" style="flex-direction: column; justify-content: center;">
-
-                <v-btn
-                 
-                  class="btn-roteiro"
-                  color="color-roteiro"
-                  @click="openRoteiro('<h1>Carolina</h1>')"
-                >
-                  Descrição
-                  <v-icon right>
-                    mdi-palm-tree
-                  </v-icon>
-                </v-btn>
-
-                <v-btn
-               
-                  color="success"
-                  class="btn-roteiro"
-                >
-                  Whatsapp
-                </v-btn>
-              
-              </div>
-            </v-row>
-            
-           </v-card-text>
-
-        </v-card>
-      
-    </v-col>
-    <!-- -->
-    <!---->
-    <v-col md="4">
-      
-        <v-card
-          class="mx-auto"
-          max-width="320"
-        >
-          <v-img
-            height="200"
-            src="https://i.pinimg.com/originals/d9/f1/5e/d9f15e78f0ac37de87d9b95b545ed03d.jpg"
-          >
-          </v-img>
-          <div class="back-pacote-cinza">
-            <v-row>
-              <div class="col-12">
-                <div class="d-flex align-center justify-space-between">
-                  <span class="pacote-data-principal">
-                    <span class="pacote-data-prox">Próxima Data:</span><br>
-                    2 maio
-                  </span>
-                  <v-btn
-                 
-                    outlined
-                    fab
-                    color="teal"
-                    
-                  >
-                    <v-icon>mdi-playlist-plus</v-icon>
-                  </v-btn>
-                </div>
-              </div>
-            </v-row>
-          </div>
-          <v-card-text>
-            <v-row>
-              <div class="col-12">
-                <p class="pacote-p pacote-title">Santo Amaro</p>
-                <p class="pacote-p pacote-sub">Saindo de <span class="pacote-cidade">São Luís</span></p>
-              </div>
-            </v-row>
-            <v-row>
-              <div class="col-12">
-                <ul class="pacote-list-itens" >
-                  <li>
-                    <span><v-icon class="icon-color">mdi-clock</v-icon></span>
-                    1 Dia
-                  </li>
-                  <li>
-                    <span><v-icon class="icon-color">mdi-clock</v-icon></span>
-                    1 Dia
-                  </li>
-                  <li>
-                    <span><v-icon class="icon-color">mdi-clock</v-icon></span>
-                    1 Dia
-                  </li>
-                </ul>
-              </div>
-            </v-row>
-            <v-row>
-              
-            </v-row>
-            <v-row>
-              
-              <div class="col-6 d-flex pacote-preco" style="flex-direction: column; justify-content: center;">
-                <span class="pacote-preco-item">Individual<br><span class="pacote-preco-tipo"><span class="pacote-preco-moeda">R$</span> 2900</span></span>
-                <span class="pacote-preco-item">Casadinha<br><span class="pacote-preco-tipo"><span class="pacote-preco-moeda">R$</span> 2900</span></span>
               </div>
               <div class="col-6 d-flex align-center" style="flex-direction: column; justify-content: center;">
 
@@ -668,7 +680,7 @@ export default {
   font-weight: 500;
 }
 .pacote-preco-item{
-  padding: 5px 6px;
+  padding: 5px 0px;
   font-weight: 600;
   color: rgb(80 79 79 / 60%);
 }
@@ -681,12 +693,13 @@ export default {
 }
 .pacote-preco-tipo{
    color: #d88304;
-   font-size: 21px;
+   font-size: 22px;
 }
 .pacote-title{
-  font-size: 22px;
+  font-size: 19px;
   color: #565656;
   font-weight: 600;
+  line-height: 1.2;
 }
 .pacote-sub{
   color: #435b71;
@@ -698,7 +711,7 @@ export default {
   min-width: 142px !important;
 }
 span.pacote-preco-moeda {
-  font-size: 17px;
+  font-size: 16px;
 }
 .pacote-cidade{
   font-weight: 600;
